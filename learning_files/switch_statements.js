d22c3f3840332c32bdb352af12da83776f4a0794
gato365 <immanuelwilliams@gato365.com>
2022-12-29 03:27:12 -0800

    Basic understanding of switch statements

---
+++ learning_files/switch_statements.js
@@ -0,0 +1,16 @@
+
+
+
+
+const testInfo = 'help'
+
+switch (testInfo) {
+    case 'help':
+        console.log('1. Get lost!')
+        break;
+
+    default:
+        console.log('2. Keep Going')
+        break;
+}
+

aceb325f7a31b5396630fbc5c4639a25cc233bff
gato365 <immanuelwilliams@gato365.com>
2023-01-01 04:56:23 -0800

    More info

--- dist/generateHTML.js
+++ dist/generateHTML.js
@@ -11,16 +11,16 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        </head>
 
+        <body>
+        
         <!-- Name of Page -->
     <title>${teamInfo.____} Page </title>
     <!-- Insert Manager Info -->
     ${displayEngineers(teamInfo.___)}
     <!-- Insert Interns Info -->
     ${displayInterns(teamInfo.___)}
-
-
-    
 
 </body>
 

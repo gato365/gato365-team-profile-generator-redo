efaad0a4b8788493faabb5909c3b1c6d55bfaa99
gato365 <immanuelwilliams@gato365.com>
2023-01-05 02:43:44 -0800

    Setting up Test to verify Employee class works

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,23 @@
+const Employee = require("../lib/Employee");
+
+
+describe('ClassName', () => {
+    test('methodName', () => {
+        // Test code
+    });
+});
+
+test("Sample Test", async() =>{
+
+    // Setup 
+    const name = "James Williams";
+    const id = "123";
+    const email = "james@williams.com";
+
+    // Running the Code
+    const employee = new Employee();
+
+    // Verify the Result
+
+
+});

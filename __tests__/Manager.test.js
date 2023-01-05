56ca64725980396a6915b0960fef2d161637b046
gato365 <immanuelwilliams@gato365.com>
2023-01-05 03:25:47 -0800

    Added manager test

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,5 +1,90 @@
 const generateHTML = require('../lib/Manager');
 
-// const def = new Manager('def', 1, 'def@def.com', 11);
+test("We can create a Manager with name, id, and email", async () => {
 
-// console.log(def.getOfficeNumber())
+    // Setup 
+    const name = "Rock Williams";
+    const id = "1323";
+    const email = "rock@williams.com";
+    const officeNumber = "226554";
+    const teamInfo = "WWE";
+    // Running the Code
+    const manager = new Manager(name, id, email, officeNumber,teamInfo);
+
+    // Verify the Result
+    expect(manager.name).toBe(name);
+    expect(manager.id).toBe(id);
+    expect(manager.email).toBe(email);
+
+
+
+});
+
+describe("This Describe Block is for Manager Functions", () => {
+
+    // Office Number Setup
+    test("We are looing for a good Office Number", async () => {
+
+        // Setup 
+        const name = "Rock Williams";
+        const id = "1323";
+        const email = "rock@williams.com";
+        const officeNumber = "226554";
+        const teamInfo = "WWE";
+        const manager = new Manager(name, id, email, officeNumber,teamInfo);
+
+        // Running the Code
+        const result = manager.getOfficeNumber();
+
+        // Verify the Result
+        expect(result).toBe(officeNumber);
+
+
+    });
+
+        // Team Info Setup
+        test("We are looing for a good Team Info", async () => {
+
+            // Setup 
+            const name = "Rock Williams";
+            const id = "1323";
+            const email = "rock@williams.com";
+            const officeNumber = "226554";
+            const teamInfo = "WWE";
+            const manager = new Manager(name, id, email, officeNumber,teamInfo);
+    
+            // Running the Code
+            const result = manager.getTeamInfo();
+    
+            // Verify the Result
+            expect(result).toBe(teamInfo);
+    
+    
+        });
+    
+
+
+
+
+    // Role Setup
+    test("We are looing for a good role", async () => {
+
+        // Setup 
+        const name = "Rock Williams";
+        const id = "1323";
+        const email = "rock@williams.com";
+        const officeNumber = "226554";
+        const manager = new Manager(name, id, email, officeNumber);
+
+        // Running the Code
+        const result = manager.getRole();
+
+        // Verify the Result
+        expect(result).toBe('Manager');
+
+
+    });
+
+
+
+});
